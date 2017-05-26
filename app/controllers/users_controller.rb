f495c111db5b252578b27bcd7ff578c17758f73e
keisuke-yoshigai <KeisukeYoshigai@KeisukeYoshigai-no-MacBook-Air.local>
2017-05-26 20:15:51 +0900

    Display flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,25 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:edit, :update]
+
   def edit
     @user = User.find(params[:id])
   end
 
   def update
-    user = User.find(params[:id])
-    if user.id == current_user.id
-      user.update(strong_parameter)
+    @user.update(strong_parameter)
+    if @user.save
+      redirect_to root_path
+    else
+      redirect_to new_user_session_path
     end
-    redirect_to controller: :messages, action: :index
   end
 
   private
   def strong_parameter
     params.require(:user).permit(:name, :email)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
