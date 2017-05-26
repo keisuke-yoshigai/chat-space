ae66211ba0a8507397857b21cde380d30d54a619
keisuke-yoshigai <KeisukeYoshigai@KeisukeYoshigai-no-MacBook-Air.local>
2017-05-26 11:44:38 +0900

    Add function of editing user information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.update(strong_parameter)
+    redirect_to controller: :messages, action: :index
+  end
+
+  private
+  def strong_parameter
+    params.require(:user).permit(:name, :email)
+  end
 end
