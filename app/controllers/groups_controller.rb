b181e82e59e424f173fe2cd24848819450e88f55
keisuke-yoshigai <KeisukeYoshigai@KeisukeYoshigai-no-MacBook-Air.local>
2017-05-28 20:47:39 +0900

    Show error messages and refactoring

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,38 @@
 class GroupsController < ApplicationController
+  before_action :group_find, only: [:edit, :update]
+
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.create(strong_parameter)
+    if @group.save
+      redirect_to controller: :messages, action: :index
+    else
+      render "new"
+    end
   end
 
   def edit
   end
 
   def update
+    @group.update(strong_parameter)
+    if @group.save
+      redirect_to group_messages_path(params[:id])
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def group_find
+    @group = Group.find(params[:id])
+  end
+
+  def strong_parameter
+    params.require(:group).permit(:name)
   end
 end
