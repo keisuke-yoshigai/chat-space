86609bdb1a72cbdb5b1855af066a6a2c9d9e052e
keisuke-yoshigai <KeisukeYoshigai@KeisukeYoshigai-no-MacBook-Air.local>
2017-06-01 17:00:17 +0900

    Refactoring

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,14 @@
 class MessagesController < ApplicationController
+  before_action :find_group, only: [:index, :create]
+
   def index
-    @group = Group.find(params[:group_id])
-    # .includes(group_users: :user)
     @message = Message.new
   end
 
-  def new
-  end
-
   def create
-    @group = Group.find(params[:group_id])
     @message = @group.messages.new(message_params)
     if @message.save
-      flash[:notice] = "メッセージを作成しました"
-      redirect_to group_messages_path(params[:group_id])
+      redirect_to group_messages_path(params[:group_id]), notice: "メッセージを作成しました"
     else
       flash.now[:alert] = "メッセージ作成に失敗しました"
       render "index"
@@ -21,6 +16,10 @@ class MessagesController < ApplicationController
   end
 
   private
+
+  def find_group
+    @group = Group.find(params[:group_id])
+  end
 
   def message_params
     params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
