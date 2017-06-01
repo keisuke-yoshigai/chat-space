cc7a9919a67271b681b107641531f0abc5e4ed52
keisuke-yoshigai <KeisukeYoshigai@KeisukeYoshigai-no-MacBook-Air.local>
2017-06-01 13:10:17 +0900

    Edit create method and message_params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,21 @@
 class MessagesController < ApplicationController
   def index
-    @group = Group.find(params[:group_id]).includes(group_user: :users)
+    @group = Group.find(params[:group_id])
+    # .includes(group_users: :user)
+    @message = Message.new
+  end
+
+  def new
+  end
+
+  def create
+    @message = Message.create(message_params)
+    redirect_to group_messages_path(params[:group_id])
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id])
   end
 end
