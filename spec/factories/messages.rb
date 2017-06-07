807588fd4007f153fbba77fb7d9f25978bc94d5a
keisuke-yoshigai <KeisukeYoshigai@KeisukeYoshigai-no-MacBook-Air.local>
2017-06-07 20:11:29 +0900

    Add invalid factory

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,8 +1,10 @@
-require "date"
-
 FactoryGirl.define do
   factory :message do
     body          "sample message"
     image           { Rack::Test::UploadedFile.new('spec/test.jpg', 'image/jpg') }
+    factory :invalid_message do
+      body        nil
+      image        nil
+    end
   end
 end
